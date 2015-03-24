b32951c1c7fe3e3350ceffd3f7b2335c0dafb9f1
David Cavins <david.cavins@gmail.com>
2015-03-24 09:25:17 -0500

    Use keyup instead of blur to test email match
    Gives instant feedback to the user whether there’s a match or not.

--- public/assets/js/dev/email-compare.js
+++ public/assets/js/dev/email-compare.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 	jQuery("#signup_email_confirm").wrap('<div id="email-comparer" class="ajax-validation"></div> ');
 	jQuery("#email-comparer").append("<span id='email-comparison-message'></span> ");
 
-	jQuery("#signup_email_confirm").on( "blur", function(){
+	jQuery("#signup_email_confirm").on( "keyup", function(){
 		jQuery("#signup_email_confirm").val( jQuery.trim( jQuery("#signup_email_confirm").val() ) );
 		var first_value = jQuery("#signup_email").val();
 		var second_value = jQuery("#signup_email_confirm").val();
@@ -12,7 +12,9 @@ jQuery(document).ready(function(){
 			jQuery('#email-comparison-message').addClass( 'error' );
 			jQuery('#email-comparison-message').css({display:'block'});
 		} else {
-			jQuery('#email-comparison-message').hide();
+			jQuery('#email-comparison-message').empty().html( 'The addresses you entered match.' );
+			jQuery('#email-comparison-message').addClass( 'validated' );
+			jQuery('#email-comparison-message').css({display:'block'});
 		}
 	});
 });
